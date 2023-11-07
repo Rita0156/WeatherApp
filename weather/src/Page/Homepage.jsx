e1c8a3b2657d6643d2767497ea24681dd18c7e29
Rita0156 <ritachaudhari006@gmail.com>
2023-11-07 23:13:58 +0530

    input

---
+++ weather/src/Page/Homepage.jsx
@@ -0,0 +1,14 @@
+
+const Homepage=()=>{
+    return (
+        <div>
+            <h1>Weather app</h1>
+            <div>
+                <input type="text" placeholder="enter city"/>
+                <button type="submit">Search Weather</button>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
