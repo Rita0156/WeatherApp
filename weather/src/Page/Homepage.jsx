a3c83e5325fa7b1b39365a8c62ba75d402026f19
Rita0156 <ritachaudhari006@gmail.com>
2023-11-08 12:03:01 +0530

    mnb

--- weather/src/Page/Homepage.jsx
+++ weather/src/Page/Homepage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Mapfunc from "../component/Map"
 const Homepage=()=>{
+   // box-shadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset";
     //api=`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
     const [city,setCity]=useState("")
     const [data,setData]=useState([])
@@ -8,6 +9,12 @@ const Homepage=()=>{
     const [lon,setLon]=useState("")
     const [lat,setLet]=useState("")
     const [iframe,setiframe]=useState("")
+    //const [cur,setCur]=useState("")
+    const [max,setMax]=useState("")
+    const [min,setMin]=useState("")
+    const [rise,setRise]=useState("")
+    const [set,setSet]=useState("")
+    //const temp=''
      const api_key="3f9eb30a8c0b0d47f7cbdf5d16ba8100"
      const fetchReq=async()=>{
        await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
@@ -17,6 +24,11 @@ const Homepage=()=>{
         .then((res)=>{
             console.log(res)
             setData(res)
+            
+            setMax(res.main.temp_max)
+            setMin(res.main.temp_min)
+            setRise(res.sys.sunrise)
+            setSet(res.sys.sunset)
             const iSrc=`https://maps.google.com/maps?q=${res.name},%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed`
             setiframe(iSrc)
         })
@@ -42,33 +54,92 @@ const Homepage=()=>{
     }
     useEffect(()=>{
            fetchReq()
-    },[])
+           getGeocur()
+           handalCity()
+    },[city])
     return (
-        <div>
+        <div style={{backgroundImage:"url(https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?cs=srgb&dl=pexels-pixabay-209831.jpg&fm=jpg)"}} >
             <h1>Weather app</h1>
-            <div>
+            <div style={{width:"90%",display:"flex",margin:"auto",justifyContent:"space-between",paddingLeft:"20px",paddingRight:"20px",paddingTop:"0px",paddingBottom:"0px",height:"20px"}}>
+                <div style={{display:"flex"}}>
+                <h2>City :</h2>
                 <input onChange={(e)=>setCity(e.target.value)} type="text" placeholder="enter city"/>
+                </div>
                 <button onClick={handalCity} type="submit">Search Weather</button>
             </div>
-            <div>
+            <div style={{backgroundImage:"url(https://img.freepik.com/free-vector/dark-watercolor-background-design_1034-738.jpg)",width:"50%"}}>
+                    <h1 style={{color:"white"}}>City : {data.name}</h1>
                 
-                  {/*data &&  <Mapfunc name={data.name} cur={data.main.temp} max={data.main.max_temp} min={data.main.min_temp} />*/}
-               
+                  <div style={{display:"flex",justifyContent:"space-around", backgroundColor:"whitesmoke"}}>
+                    
+                      <h2>min temp - {min}</h2>
+                      <h2>max temp - {max}</h2>
+                  </div>
+                  <div style={{display:"flex"}}>
+                     <div>
+                     <h3>Sunrise : {}</h3>
+                     <h3>Sunset : {}</h3>
+                     </div>
+                     <div>
+                        <img style={{borderRadius:"70px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQThRcEATwQkIxWTrZ43f6XnhKTmjYVDrRJkQ&usqp=CAU"/>
+                     </div>
+                  </div>
             </div>
             <div>
 
             <div class="mapouter">
                 <div class="gmap_canvas">
                     <iframe width="600" height="500" id="gmap_canvas" src={iframe} frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                    <a href="https://embedgooglemap.net/124/">
-                        </a><br/>
-                        
-                        <a href="https://www.embedgooglemap.net"></a>
+                    
                         
                         </div>
                         </div>
 
 
+            </div>
+            <div style={{display:"flex",width:"95%",margin:"auto",justifyContent:"space-between"}}>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Sun</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://pnglib.nyc3.cdn.digitaloceanspaces.com/uploads/2020/11/sun-and-cloud-png_5fb26f319afd4.png"/>
+                    <h3>31.34'C</h3>
+                    <h3>23.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}} >
+                    <h3>Mon</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <h3>28.34'C</h3>
+                    <h3>20.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Tue</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <h3>40.34'C</h3>
+                    <h3>35.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Wed</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <h3>31.34'C</h3>
+                    <h3>23.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Thu</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <h3>27.34'C</h3>
+                    <h3>24.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Fri</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <h3>30.34'C</h3>
+                    <h3>25.15'C</h3>
+                </div>
+                <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
+                    <h3>Sat</h3>
+                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <h3>29.34'C</h3>
+                    <h3>18.15'C</h3>
+                </div>
             </div>
 
             
