4ca551ee57ca2a6fe089673f9fed68558a4d5b8b
Rita0156 <ritachaudhari006@gmail.com>
2023-11-10 22:01:57 +0530

    deleted

--- weather/src/Page/Homepage.jsx
+++ weather/src/Page/Homepage.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react"
 import Mapfunc from "../component/Map"
 const Homepage=()=>{
-   // box-shadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset";
-    //api=`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+    const images=["https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Weather-heavy-overcast.svg/2048px-Weather-heavy-overcast.svg.png",
+                 "https://iconape.com/wp-content/png_logo_vector/rainy-weather-symbol-logo.png",
+                 "https://clipart-library.com/data_images/193485.png"
+    ]
+    const [src,setSrc]=useState("")
+       // box-shadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset";
+       //api=`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
     const [city,setCity]=useState("")
     const [data,setData]=useState([])
-    //const [cur,setcur]=useState("")
+       //const [cur,setcur]=useState("")
     const [lon,setLon]=useState("")
     const [lat,setLet]=useState("")
     const [iframe,setiframe]=useState("")
-    //const [cur,setCur]=useState("")
+       //const [cur,setCur]=useState("")
     const [max,setMax]=useState("")
     const [min,setMin]=useState("")
     const [rise,setRise]=useState("")
     const [set,setSet]=useState("")
-    //const temp=''
+      //const temp=''
      const api_key="3f9eb30a8c0b0d47f7cbdf5d16ba8100"
      const today=new Date();
      const month = today.getMonth()+1;
      const year = today.getFullYear();
      const date = today. getDate();
-     const currentDate = month + "/" + date + "/" + year;
+     const day=today.getDay()
+     const currentDate = date + "/" + month + "/" + year;
      const fetchReq=()=>{
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&q=${city}&appid=${api_key}`)
         .then((req)=>{
-            return req.json()
+            return req.json();
         })
         .then((res)=>{
             console.log(res)
@@ -36,6 +42,15 @@ const Homepage=()=>{
         const Fmax=((Number(mMax)-32)*(5/9)-50)
             setMin(((Fmin-32)*0.57).toFixed(2))
             setMax(((Fmax-32)*0.57).toFixed(2))
+            if(max<20){
+                setSrc("https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Weather-heavy-overcast.svg/2048px-Weather-heavy-overcast.svg.png")
+            }
+            else if (max>20 && max<35){
+                setSrc("https://iconape.com/wp-content/png_logo_vector/rainy-weather-symbol-logo.png")
+            }
+            else{
+                setSrc("https://clipart-library.com/data_images/193485.png")
+            }
             setRise(res.sys.sunrise)
             setSet(res.sys.sunset)
             const iSrc=`https://maps.google.com/maps?q=${res.name},%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed`
@@ -63,8 +78,14 @@ const Homepage=()=>{
     }
     useEffect(()=>{
            fetchReq()
-           getGeocur()
-           handalCity()
+           
+           
+    },[])
+    useEffect(()=>{
+        getGeocur()
+    },[])
+    useEffect(()=>{
+         handalCity()
     },[])
     return (
         <div style={{backgroundImage:"url(https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?cs=srgb&dl=pexels-pixabay-209831.jpg&fm=jpg)",paddingTop:"20px"}} >
@@ -81,7 +102,7 @@ const Homepage=()=>{
             <div key={data.id} style={{backgroundColor:"teal",width:"50%",padding:"20px"}}>
                     <h1 style={{color:"white"}}>City : {data.name}</h1>
                     <h3 style={{color:"white"}}>Date :-{currentDate}</h3>
-                
+                    
                   <div style={{display:"flex",justifyContent:"space-around", backgroundColor:"whitesmoke",marginBottom:"20px"}}>
                     
                       <h2>min temp - {min}°C</h2>
@@ -113,43 +134,43 @@ const Homepage=()=>{
             <div style={{display:"flex",width:"95%",margin:"auto",justifyContent:"space-between"}}>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Sun</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://pnglib.nyc3.cdn.digitaloceanspaces.com/uploads/2020/11/sun-and-cloud-png_5fb26f319afd4.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>31.34 °C</h3>
                     <h3>23.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}} >
                     <h3>Mon</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>28.34 °C</h3>
                     <h3>20.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Tue</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>40.34 °C</h3>
                     <h3>35.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Wed</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>31.34 °C</h3>
                     <h3>23.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Thu</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>27.34 °C</h3>
                     <h3>24.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Fri</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://assets.stickpng.com/images/5f48f8e868ecc70004ae6f8b.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>30.34 °C</h3>
                     <h3>25.15 °C</h3>
                 </div>
                 <div style={{backgroundColor:"rgba(0, 0, 0, 0.56)",padding:"20px",color:"white"}}>
                     <h3>Sat</h3>
-                    <img style={{width:"100px",height:"100px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Cartoon_cloud.svg/320px-Cartoon_cloud.svg.png"/>
+                    <img style={{width:"100px",height:"100px"}} src={src}/>
                     <h3>29.34 °C</h3>
                     <h3>18.15 °C</h3>
                 </div>
@@ -161,3 +182,5 @@ const Homepage=()=>{
 }
 
 export default Homepage
+
+
